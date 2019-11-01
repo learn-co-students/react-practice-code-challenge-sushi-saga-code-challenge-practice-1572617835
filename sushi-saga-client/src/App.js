a1f10d6489b4ce3c295fe9ed109b8c11b1484c15
github username <github email address>
2019-11-01 11:58:26 -0400

    lame

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,59 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      sushis: [],
+      money: 100,
+    }
+  }
+
+  // sushis = this.state.sushis
+
+  // sliceSushi = ()=>{
+  //   let sushis = [...this.state.sushis]
+  //   sushis.slice(0,3)
+  // }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushiData => 
+    // sushiData is an array of sushis
+    // ((sushiData)=> {
+    //   sus
+    // })
+      this.setState({
+        sushis: sushiData
+      },()=>{
+        console.log(this.state.sushis)
+      })
+    )
+  }
+
+  // change state of how many sushis are on the screen
+
+
+  // change state of money when user clicks on sushi
+
+
+  handleMoreClick(event){
+    // if(event.target)
+    console.log('i was clicked')
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+        sushis={this.state.sushis}
+        handleMoreClick={this.handleMoreClick()}
+        // need to pass prop to the more button
+        />
+        <Table
+        money={this.state.money}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -2,15 +2,31 @@ import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
 
 const SushiContainer = (props) => {
+
+  const sushiData = props.sushis
+
+  // renderMoreSushi =
+
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+          {sushiData.slice(0,4).map(sushi => 
+          <div key={sushi.id}>
+              <img 
+              className="plate"
+              src={sushi.img_url} 
+              alt=""></img>
+            <strong>
+              <p>
+              {sushi.name} - ${sushi.price}
+              </p>
+            </strong>
+          </div>
+          )}
+        <MoreButton 
+        handleMoreClick={props.handleMoreClick}
+        />
       </div>
     </Fragment>
   )
