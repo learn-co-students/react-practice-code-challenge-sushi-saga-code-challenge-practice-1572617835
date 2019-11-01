bde5b6b6ae24012f9177edc4a79845cbdb06463d
Evan Krone <evankrone@Evans-MacBook-Pro-2.local>
2019-11-01 11:58:44 -0400

    evan

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,13 +3,43 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis/?results=4"
 
 class App extends Component {
+constructor(){
+  super();
+  this.state = {
+    sushiArray: []
+  };
+}
+
+componentDidMount(){
+fetch(API)
+.then(resp => resp.json())
+.then(data => {
+//   data.forEach(su => { 
+//   if (sushiArray.length < 4){
+//     sushiArray.push(su);
+//   }
+// })
+  let sushiArray = data.map((s) => {
+    return(
+      <div key={s.id}>
+        <img src={s.img_url}/>
+        <h1>{s.name}</h1>
+      </div>
+    )
+  })
+  this.setState({sushiArray: sushiArray});
+})
+}
 
   render() {
     return (
       <div className="app">
+        <div className='sushi'>
+          {this.state.sushiArray}
+        </div>
         <SushiContainer  />
         <Table />
       </div>
