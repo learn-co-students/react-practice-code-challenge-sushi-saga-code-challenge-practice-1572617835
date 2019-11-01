669cf03eb443e008a029c315539e4e0f432ed525
github username <github email address>
2019-11-01 12:01:42 -0400

    hard

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sushiArray: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        let pictures = data.map(pic => {
+          return (
+            <div key={pic.results}>
+              <img src={pic.picture} />
+            </div>
+          );
+        });
+        this.setState({ pictures: pictures });
+        console.log("state", this.state.pictures);
+      });
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer />
         <Table />
       </div>
     );
